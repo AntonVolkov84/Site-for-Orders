2a4e2eb1ebc1812ca0dc13ffc94234faf8cfa502
Anton Volkov <antvolkov84@gmail.com>
2024-12-12 20:50:20 +0200

    add translate 3lang

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import "./Header.css";
 import logo from "../Orders3.png";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 function Header() {
+  const { t } = useTranslation();
   return (
     <header className="header">
       <div className="header-blockimg">
@@ -11,13 +13,13 @@ function Header() {
       </div>
       <div className="header-menu">
         <Link className="btn-home" to="/">
-          <h2 className="header-href">Home</h2>
+          <h2 className="header-href">{t("home")}</h2>
         </Link>
         <Link className="btn-language" to="language">
-          <h2 className="header-href">Language</h2>
+          <h2 className="header-href">{t("language")}</h2>
         </Link>
         <Link className="btn-confidential" to="confidential">
-          <h2 className="header-href">CONFIDENTIALITY</h2>
+          <h2 className="header-href">{t("confidentiality")}</h2>
         </Link>
       </div>
     </header>
--- src/screens/Languagescreen.jsx
+++ src/screens/Languagescreen.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { LanguageResources } from "../i18next";
+import i18next from "../i18next";
+import LanguageList from "../locales/LanguageList.json";
+import "./LanguageScreen.css";
 
 function Languagescreen() {
-  return <div className="languages"></div>;
+  const changeLng = (lng) => {
+    i18next.changeLanguage(lng);
+  };
+  return (
+    <div className="languages">
+      {Object.keys(LanguageResources).map((e, index) => (
+        <button className="languages-btn" key={index} onClick={() => changeLng(e)}>
+          {LanguageList[e].nativeName}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default Languagescreen;
